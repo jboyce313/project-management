9aa76026abfe1f2aab7de63449703b97072ee881
Aaron Wang <AWANG6@TRAVELERS.COM>
2024-02-08 15:51:11 -0500

    added initial css

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom";
+import "../styles/header.css"; // Import CSS file
 
 export function Header({ setLoggedIn }) {
   function handleClick() {
     localStorage.setItem("loggedIn", "false");
     setLoggedIn(false);
   }
+
   return (
-    <>
-      <h1>Project Management</h1>
-      {localStorage.getItem("loggedIn") === true ? (
-        <Link onClick={handleClick} to={"/"}>
-          Logout
-        </Link>
-      ) : (
-        ""
-      )}
-    </>
+    <header className="header">
+      <div className="container">
+        <h1 className="logo">Project Management</h1>
+        <p className="made-by">Made by Aaron & Jacob</p>
+        {localStorage.getItem("loggedIn") === "true" ? (
+          <Link onClick={handleClick} to={"/"} className="logout-link">
+            Logout
+          </Link>
+        ) : null}
+      </div>
+    </header>
   );
 }

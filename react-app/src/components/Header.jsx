000f2aa44798e35601db53d12442bc5df21d76a3
Jacob Boyce <jboyce313@gmail.com>
2024-02-08 15:05:19 -0600

    debug login

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
 
-export function Header({ setLoggedIn }) {
+export function Header({ setLoggedIn, setUser, user }) {
   function handleClick() {
     localStorage.setItem("loggedIn", "false");
     setLoggedIn(false);
+    setUser("");
   }
   return (
     <>
       <h1>Project Management</h1>
-      {localStorage.getItem("loggedIn") === true ? (
-        <Link onClick={handleClick} to={"/"}>
-          Logout
-        </Link>
+      {localStorage.getItem("loggedIn") === "true" ? (
+        <>
+          <p>Logged in as {user}</p>
+          <Link onClick={handleClick} to={"/"}>
+            Logout
+          </Link>
+        </>
       ) : (
         ""
       )}

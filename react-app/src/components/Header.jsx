96a5f214128f3fedea7478cdb9f6fbb628cbe6ed
Jacob Boyce <jboyce313@gmail.com>
2024-02-10 11:10:08 -0600

    edited css

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -19,16 +19,22 @@ export function Header({
   return (
     <header className="header">
       <div className="container">
-        <h1 className="logo">Project Manager</h1>
-        <p className="made-by">Made by Aaron & Jacob</p>
-        {localStorage.getItem("loggedIn") == "true" ? (
-          <Link onClick={handleClick} to={"/"} className="logout-link">
-            Logout
+        <div className="pageTitle">
+          <Link to={"/"} className="logo">
+            Project Manager
           </Link>
-        ) : null}
-        <Link className={"predictLink"} to={"/predict"}>
-          Project Completion Time Predictor
-        </Link>
+        </div>
+        {/* <p className="made-by">Made by Aaron & Jacob</p> */}
+        <div className="nav">
+          <Link className={"predictLink"} to={"/predict"}>
+            Project Completion Time Predictor
+          </Link>
+          {localStorage.getItem("loggedIn") == "true" ? (
+            <Link onClick={handleClick} to={"/"} className="logout-link">
+              Logout
+            </Link>
+          ) : null}
+        </div>
       </div>
     </header>
   );
